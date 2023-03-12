e4245c9fcfe2c37b747b23aa2125f0d9bb7e91e3
Christian Munley <chrismun@udel.edu>
2023-03-11 21:42:29 -0500

    fixed components. tests passing, should double check.

--- src/bad-components/ColoredBox.tsx
+++ src/bad-components/ColoredBox.tsx
@@ -4,23 +4,18 @@ import { Button } from "react-bootstrap";
 export const COLORS = ["red", "blue", "green"];
 const DEFAULT_COLOR_INDEX = 0;
 
-function ChangeColor(): JSX.Element {
-    const [colorIndex, setColorIndex] = useState<number>(DEFAULT_COLOR_INDEX);
-    return (
-        <Button onClick={() => setColorIndex((1 + colorIndex) % COLORS.length)}>
-            Next Color
-        </Button>
-    );
+function ChangeColor({ change }: { change: () => void }): JSX.Element {
+    return <Button onClick={change}>Next Color</Button>;
 }
 
-function ColorPreview(): JSX.Element {
+function ColorPreview({ color }: { color: number }): JSX.Element {
     return (
         <div
             data-testid="colored-box"
             style={{
                 width: "50px",
                 height: "50px",
-                backgroundColor: COLORS[DEFAULT_COLOR_INDEX],
+                backgroundColor: COLORS[color],
                 display: "inline-block",
                 verticalAlign: "bottom",
                 marginLeft: "5px"
@@ -30,13 +25,19 @@ function ColorPreview(): JSX.Element {
 }
 
 export function ColoredBox(): JSX.Element {
+    const [colorIndex, setColorIndex] = useState<number>(DEFAULT_COLOR_INDEX);
+
+    function nextColor(): void {
+        setColorIndex((colorIndex + 1) % COLORS.length);
+    }
+
     return (
         <div>
             <h3>Colored Box</h3>
-            <span>The current color is: {COLORS[DEFAULT_COLOR_INDEX]}</span>
+            <span>The current color is: {COLORS[colorIndex]}</span>
             <div>
-                <ChangeColor></ChangeColor>
-                <ColorPreview></ColorPreview>
+                <ChangeColor change={nextColor}></ChangeColor>
+                <ColorPreview color={colorIndex}></ColorPreview>
             </div>
         </div>
     );
--- src/bad-components/DoubleHalf.tsx
+++ src/bad-components/DoubleHalf.tsx
@@ -1,24 +1,46 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
-import { dhValue, setDhValue } from "./DoubleHalfState";
+//import { dhValue, setDhValue } from "./DoubleHalfState";
 
-function Doubler(): JSX.Element {
-    return <Button onClick={() => setDhValue(2 * dhValue)}>Double</Button>;
+function Doubler({
+    val,
+    f
+}: {
+    val: number;
+    f: (val: number) => void;
+}): JSX.Element {
+    return <Button onClick={() => f(val)}>Double</Button>;
 }
 
-function Halver(): JSX.Element {
-    return <Button onClick={() => setDhValue(0.5 * dhValue)}>Halve</Button>;
+function Halver({
+    val,
+    f
+}: {
+    val: number;
+    f: (val: number) => void;
+}): JSX.Element {
+    return <Button onClick={() => f(val)}>Halve</Button>;
 }
 
 export function DoubleHalf(): JSX.Element {
+    const [dhValue, setDhValue] = useState<number>(10);
+
+    const dbl = () => {
+        setDhValue(2 * dhValue);
+    };
+
+    const hlf = () => {
+        setDhValue(0.5 * dhValue);
+    };
+
     return (
         <div>
             <h3>Double Half</h3>
             <div>
                 The current value is: <span>{dhValue}</span>
             </div>
-            <Doubler></Doubler>
-            <Halver></Halver>
+            <Doubler val={dhValue} f={dbl}></Doubler>
+            <Halver val={dhValue} f={hlf}></Halver>
         </div>
     );
 }
