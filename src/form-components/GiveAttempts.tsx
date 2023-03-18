b05435aee0bcea627fe025e724b23ac7b551cc74
Christian Munley <chrismun@udel.edu>
2023-03-18 01:26:32 -0400

    uploading

--- src/form-components/GiveAttempts.tsx
+++ src/form-components/GiveAttempts.tsx
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
+import { Button } from "react-bootstrap";
 
 export function GiveAttempts(): JSX.Element {
     const [attemptsLeft, setAttemptsLeft] = useState<number>(3);
@@ -22,8 +24,25 @@ export function GiveAttempts(): JSX.Element {
         <div>
             <h3>Attempts left: {attemptsLeft}</h3>
             <Form.Group controlId="formAttempts">
-                
+                <Form.Label>Additional attempts:</Form.Label>
+                <Form.Control
+                    type="number"
+                    value={attemptsToAdd}
+                    onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+                        setAttemptsToAdd(event.target.value)
+                    }
+                />
             </Form.Group>
+            <Button
+                variant="primary"
+                onClick={handleTakeQuiz}
+                disabled={attemptsLeft <= 0}
+            >
+                Use
+            </Button>{" "}
+            <Button variant="primary" onClick={handleAddAttempts}>
+                Gain
+            </Button>
         </div>
     );
 }
--- src/form-components/MultipleChoiceQuestion.tsx
+++ src/form-components/MultipleChoiceQuestion.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function MultipleChoiceQuestion({
     options,
@@ -7,9 +8,30 @@ export function MultipleChoiceQuestion({
     options: string[];
     expectedAnswer: string;
 }): JSX.Element {
+    const [selectedAnswer, setSelectedAnswer] = useState<string>(options[0]);
+    const isCorrect = selectedAnswer === expectedAnswer;
+
+    function handleAnswerChange(event: React.ChangeEvent<HTMLSelectElement>) {
+        setSelectedAnswer(event.target.value);
+    }
+
     return (
         <div>
             <h3>Multiple Choice Question</h3>
+            <Form.Group controlId="multipleChoiceQuestionForm">
+                <Form.Label>Select an answer:</Form.Label>
+                <Form.Select
+                    value={selectedAnswer}
+                    onChange={handleAnswerChange}
+                >
+                    {options.map((option) => (
+                        <option key={option} value={option}>
+                            {option}
+                        </option>
+                    ))}
+                </Form.Select>
+            </Form.Group>
+            <div>{isCorrect ? "✔️" : "❌"}</div>
         </div>
     );
 }
