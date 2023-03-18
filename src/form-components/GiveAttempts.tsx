ccff431e20437f03645fd10237b2f720a1d81e23
chrismun <chrismun@udel.edu>
2023-03-18 00:35:59 -0400

    started

--- src/form-components/GiveAttempts.tsx
+++ src/form-components/GiveAttempts.tsx
@@ -1,9 +1,29 @@
 import React, { useState } from "react";
 
 export function GiveAttempts(): JSX.Element {
+    const [attemptsLeft, setAttemptsLeft] = useState<number>(3);
+    const [attemptsToAdd, setAttemptsToAdd] = useState<string>("");
+
+    const handleTakeQuiz = () => {
+        if (attemptsLeft > 0) {
+            setAttemptsLeft(attemptsLeft - 1);
+        }
+    };
+
+    const handleAddAttempts = () => {
+        const parsedAttemptsToAdd = parseInt(attemptsToAdd);
+        if (!isNaN(parsedAttemptsToAdd)) {
+            setAttemptsLeft(attemptsLeft + parsedAttemptsToAdd);
+            setAttemptsToAdd("");
+        }
+    };
+
     return (
         <div>
-            <h3>Give Attempts</h3>
+            <h3>Attempts left: {attemptsLeft}</h3>
+            <Form.Group controlId="formAttempts">
+                
+            </Form.Group>
         </div>
     );
 }
